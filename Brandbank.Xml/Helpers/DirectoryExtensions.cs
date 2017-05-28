abf43d489563217cc8df15d6abefb0d24fda33b8
Stuart <stuarttottle@hotmail.com>
2017-05-28 20:18:19 +0100

    Add zip extracter

--- Brandbank.Xml/Helpers/DirectoryExtensions.cs
+++ Brandbank.Xml/Helpers/DirectoryExtensions.cs
@@ -108,5 +108,46 @@ namespace Brandbank.Xml.Helpers
                 zipStream.CloseEntry();
             }
         }
+
+        public static string ExtractZipFile(this string archiveFilenameIn, string outFolder)
+        {
+            ZipFile zf = null;
+            try
+            {
+                FileStream fs = File.OpenRead(archiveFilenameIn);
+                zf = new ZipFile(fs);
+                foreach (ZipEntry zipEntry in zf)
+                {
+                    if (!zipEntry.IsFile)
+                    {
+                        continue;           // Ignore directories
+                    }
+                    var entryFileName = zipEntry.Name;
+
+                    var buffer = new byte[4096];     // 4K is optimum
+                    var zipStream = zf.GetInputStream(zipEntry);
+
+                    // Manipulate the output filename here as desired.
+                    var fullZipToPath = Path.Combine(outFolder, entryFileName);
+                    var directoryName = Path.GetDirectoryName(fullZipToPath);
+                    if (directoryName.Length > 0)
+                        Directory.CreateDirectory(directoryName);
+
+                    using (FileStream streamWriter = File.Create(fullZipToPath))
+                    {
+                        StreamUtils.Copy(zipStream, streamWriter, buffer);
+                    }
+                }
+            }
+            finally
+            {
+                if (zf != null)
+                {
+                    zf.IsStreamOwner = true; // Makes close also shut the underlying stream
+                    zf.Close(); // Ensure we release resources
+                }
+            }
+            return outFolder;
+        }
     }
 }
